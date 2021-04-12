5cf9391784f835f9631d8dd113a6a7a563a0c644
FahadShamim-TPS <fahad.shamim@tpsonline.com>
2021-04-12 17:01:54 +0500

    update customerview in default route config

--- Frontend/PaymentGateway_MVC/App_Start/RouteConfig.cs
+++ Frontend/PaymentGateway_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace PaymentGateway_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                // defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CustomerView", action = "addCustomerDetails", id = UrlParameter.Optional }
             );
         }
     }
